9c687c88d4eabc5480d7672d7af2d5851764e5ca
Oliver Mansfield <o.mansfield@amaze.com>
2016-03-18 14:45:59 +0000

    Scallys page started
    added the images. started the page.

--- code/gruntfile.js
+++ code/gruntfile.js
@@ -6,13 +6,14 @@ module.exports = function(grunt) {
 				files: {
 					'css/home.css' : 'scss/home.scss',
 					'css/skin.css' : 'scss/skin.scss',
+					'css/scallys.css' : 'scss/scallys.scss',
 					'css/tumblrFeed.css' : 'scss/tumblrFeed.scss'
 				}
 			}
 		},
 		watch: {
 			css: {
-				files: '**/*.scss',
+				files: 'scss/*.scss',
 				tasks: ['sass']
 			}
 		}
